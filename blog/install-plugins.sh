b8c5a902342185bf5a5c7c46217bae5b6ae9483e
NetzUfer <netzufer@gmail.com>
2025-04-22 23:58:35 +0200

    end, new

--- blog/install-plugins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Navigate to the blog directory
-cd "$(dirname "$0")"
-
-# Install the required Tailwind plugins
-npm install @tailwindcss/typography @tailwindcss/forms @tailwindcss/aspect-ratio
-
-echo "Tailwind plugins installed successfully!"
